4f9e2162b7a77e4d6beab7e8a49e19e8c7f9d310
Nisal-Kawshalya <ict22053@std.uwu.ac.lk>
2026-01-07 00:17:02 +0530

    Create all fectures and some CSS

---
+++ Portfolio/Controllers/ProjectsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        public IActionResult YummyFeels()
+        {
+            return View();
+        }
+
+        public IActionResult SkillNet()
+        {
+            return View();
+        }
+
+        public IActionResult ServeLanka()
+        {
+            return View();
+        }
+    }
+}
